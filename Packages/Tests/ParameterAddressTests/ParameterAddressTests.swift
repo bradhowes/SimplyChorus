5dfe32fc99fbee3640d5a26b0a76f11657a91a36
Brad Howes <bradhowes@mac.com>
2022-04-07 19:40:17 +0200

    Fixed unit tests

--- Packages/Tests/ParameterAddressTests/ParameterAddressTests.swift
+++ Packages/Tests/ParameterAddressTests/ParameterAddressTests.swift
@@ -4,46 +4,32 @@ import XCTest
 final class ParameterAddressTests: XCTestCase {
 
   func testParameterAddress() throws {
-    XCTAssertEqual(ParameterAddress.depth.rawValue, 0)
-    XCTAssertEqual(ParameterAddress.odd90.rawValue, 7)
-    XCTAssertEqual(ParameterAddress.allCases.count, 8)
+    XCTAssertEqual(ParameterAddress.rate.rawValue, 0)
+    XCTAssertEqual(ParameterAddress.odd90.rawValue, 5)
+    XCTAssertEqual(ParameterAddress.allCases.count, 6)
   }
 
   func testParameterDefinitions() throws {
-    let depth = ParameterAddress.depth.parameterDefinition
-    XCTAssertEqual(depth.range.lowerBound, 0.0)
-    XCTAssertEqual(depth.range.upperBound, 50.0)
-    XCTAssertEqual(depth.unit, .milliseconds)
-    XCTAssertTrue(depth.ramping)
-    XCTAssertTrue(depth.logScale)
+    let rate = ParameterAddress.rate.parameterDefinition
+    XCTAssertEqual(rate.range.lowerBound, 0.01)
+    XCTAssertEqual(rate.range.upperBound, 20.0)
+    XCTAssertEqual(rate.unit, .hertz)
+    XCTAssertTrue(rate.ramping)
+    XCTAssertFalse(rate.logScale)
 
     let delay = ParameterAddress.delay.parameterDefinition
     XCTAssertEqual(delay.range.lowerBound, 0.0)
     XCTAssertEqual(delay.range.upperBound, 50.0)
     XCTAssertEqual(delay.unit, .milliseconds)
     XCTAssertTrue(delay.ramping)
-    XCTAssertTrue(delay.logScale)
+    XCTAssertFalse(delay.logScale)
 
-    let rate = ParameterAddress.rate.parameterDefinition
-    XCTAssertEqual(rate.range.lowerBound, 0.01)
-    XCTAssertEqual(rate.range.upperBound, 20.0)
-    XCTAssertEqual(rate.unit, .hertz)
-    XCTAssertTrue(rate.ramping)
-    XCTAssertTrue(rate.logScale)
-
-    let feedback = ParameterAddress.feedback.parameterDefinition
-    XCTAssertEqual(feedback.range.lowerBound, 0.0)
-    XCTAssertEqual(feedback.range.upperBound, 100.0)
-    XCTAssertEqual(feedback.unit, .percent)
-    XCTAssertTrue(feedback.ramping)
-    XCTAssertFalse(feedback.logScale)
-
-    let negativeFeedback = ParameterAddress.negativeFeedback.parameterDefinition
-    XCTAssertEqual(negativeFeedback.range.lowerBound, 0.0)
-    XCTAssertEqual(negativeFeedback.range.upperBound, 1.0)
-    XCTAssertEqual(negativeFeedback.unit, .boolean)
-    XCTAssertFalse(negativeFeedback.ramping)
-    XCTAssertFalse(negativeFeedback.logScale)
+    let depth = ParameterAddress.depth.parameterDefinition
+    XCTAssertEqual(depth.range.lowerBound, 0.0)
+    XCTAssertEqual(depth.range.upperBound, 100.0)
+    XCTAssertEqual(depth.unit, .percent)
+    XCTAssertTrue(depth.ramping)
+    XCTAssertFalse(depth.logScale)
 
     let odd90 = ParameterAddress.odd90.parameterDefinition
     XCTAssertEqual(odd90.range.lowerBound, 0.0)
@@ -61,7 +47,7 @@ final class ParameterAddressTests: XCTestCase {
     XCTAssertEqual(parameter.identifier, definition.identifier)
     XCTAssertEqual(parameter.displayName, definition.localized)
     XCTAssertTrue(parameter.flags.contains(.flag_CanRamp))
-    XCTAssertTrue(parameter.flags.contains(.flag_DisplayLogarithmic))
+    XCTAssertFalse(parameter.flags.contains(.flag_DisplayLogarithmic))
 
     address = .odd90
     definition = address.parameterDefinition
--- Packages/Tests/ParametersTests/AudioUnitParametersTests.swift
+++ Packages/Tests/ParametersTests/AudioUnitParametersTests.swift
@@ -1,4 +1,6 @@
 import XCTest
+import AUv3Support
+import ParameterAddress
 @testable import Parameters
 import Kernel
 
@@ -11,27 +13,23 @@ class MockParameterHandler: AUParameterHandler {
 final class FilterPresetTests: XCTestCase {
 
   func testParameterAddress() throws {
-    XCTAssertEqual(ParameterAddress.depth.rawValue, 0)
-    XCTAssertEqual(ParameterAddress.odd90.rawValue, 7)
+    XCTAssertEqual(ParameterAddress.rate.rawValue, 0)
+    XCTAssertEqual(ParameterAddress.odd90.rawValue, 5)
 
     // Unfortunately, there is no init? for Obj-C enums
     // XCTAssertNil(ParameterAddress(rawValue: ParameterAddress.odd90.rawValue + 1))
 
-    XCTAssertEqual(ParameterAddress.allCases.count, 8)
+    XCTAssertEqual(ParameterAddress.allCases.count, 6)
     XCTAssertTrue(ParameterAddress.allCases.contains(.depth))
     XCTAssertTrue(ParameterAddress.allCases.contains(.rate))
     XCTAssertTrue(ParameterAddress.allCases.contains(.delay))
-    XCTAssertTrue(ParameterAddress.allCases.contains(.feedback))
-    XCTAssertTrue(ParameterAddress.allCases.contains(.dryMix))
-    XCTAssertTrue(ParameterAddress.allCases.contains(.wetMix))
-    XCTAssertTrue(ParameterAddress.allCases.contains(.negativeFeedback))
+    XCTAssertTrue(ParameterAddress.allCases.contains(.dry))
+    XCTAssertTrue(ParameterAddress.allCases.contains(.wet))
     XCTAssertTrue(ParameterAddress.allCases.contains(.odd90))
   }
 
   func testParameterDefinitions() throws {
-    let handler = MockParameterHandler()
-    let aup = AudioUnitParameters(parameterHandler: handler)
-
+    let aup = AudioUnitParameters()
     for (index, address) in ParameterAddress.allCases.enumerated() {
       XCTAssertTrue(aup.parameters[index] == aup[address])
     }
